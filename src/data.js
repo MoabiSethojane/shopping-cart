8bf9ff6a2bc114af5083c30989c2dc1afdd57659
unknown <MoabiSethojane>
2022-06-10 15:44:00 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+const data = {
+    productData:[
+        {
+id: 1,
+img: img1,
+title: 'Nike Air Yeezy 2',
+desc: 'Red October',
+price:  3999.00
+        },
+
+        {
+            id: 2,
+            img: img2,
+            title: 'Nike Air Yeezy 2',
+            desc: 'Midnight',
+            price:  849.00
+                    },
+
+                    {
+                        id: 3,
+                        img: img3,
+                        title: 'Nike Air Yeezy 2',
+                        desc: 'Wolf',
+                        price:  789.00
+                                }
+    ],
+};
+
+
+export default data;
